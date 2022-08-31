0b752f821c427dcf96a4501b8a5b3ec55fe09861
viktorkulygin <Adolf1488)>
2022-08-31 15:29:06 +0300

    practice2 task2 implemented

---
+++ practice2/js/filmsInfo.js
@@ -0,0 +1,28 @@
+let numberOfFilms = '';
+while(numberOfFilms === null || numberOfFilms.length === 0) {
+    numberOfFilms = prompt('How much films did you watch?', '');
+}
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+for(let i = 0; i < 2; i++) {
+    let filmName = '';
+    let filmRating = '';
+    while(filmName === null || filmName.length === 0 || filmName.length > 50) {
+        filmName = prompt('Write name of the one last film you watched', '');
+    }
+    while(filmRating === null || filmRating.length === 0 || filmRating.length > 50) {
+        filmRating = prompt('Which is your rating of this film? (from 0.0 to 10.0');
+    }
+    
+    personalMovieDB.movies[filmName] = filmRating;
+}
+
+document.write(JSON.stringify(personalMovieDB));
+
