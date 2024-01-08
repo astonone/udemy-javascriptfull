1984a8dd74ae5a84ea81b1f310f8c7b0bfd9d685
viktor.kulygin <viktor.kulygin@coolbet.com>
2024-01-08 11:42:26 +0200

    practice23 added service

---
+++ practice23/src/services/MarvelService.js
@@ -0,0 +1,23 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=b25538f8b037222e67f8e9d911e130ff';
+    getResource = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+    }
+}
+
+export default MarvelService;
