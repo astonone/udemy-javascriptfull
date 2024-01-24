1170fd2815631eb0c8dbdc3eb498175a510b336b
viktor.kulygin <viktor.kulygin@coolbet.com>
2024-01-24 14:31:14 +0200

    practice23 added rendering server data in the component, error handling, spinner, data transformation

---
+++ practice23/src/components/spinner/Spinner.js
@@ -0,0 +1,10 @@
+import './spinner.scss';
+import spinner from "../../resources/img/Fidget-spinner.gif";
+
+const Spinner = () => {
+    return (
+        <img src={spinner} alt="spinner" className="spinner"/>
+    )
+}
+
+export default Spinner;
