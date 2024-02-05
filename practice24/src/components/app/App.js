c7be5a843b5b90a8d6a77094d0e1e35803861e03
viktor.kulygin <viktor.kulygin@coolbet.com>
2024-02-05 13:26:11 +0200

    practice24 lazy loading

--- practice24/src/components/app/App.js
+++ practice24/src/components/app/App.js
@@ -1,9 +1,13 @@
 import AppHeader from "../appHeader/AppHeader";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import MainPage from "../../pages/MainPage";
-import ComicsPage from "../../pages/ComicsPage";
-import Error404Page from "../../pages/Error404Page";
-import SingleComicPage from "../../pages/SingleComicPage";
+
+import {lazy, Suspense} from "react";
+import Spinner from "../spinner/Spinner";
+
+const Error404Page = lazy(() => import('../../pages/Error404Page'));
+const MainPage = lazy(() => import('../../pages/MainPage'));
+const ComicsPage = lazy(() => import('../../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../../pages/SingleComicPage'));
 
 const App = () => {
 
@@ -12,12 +16,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                        <Route path="*" element={<Error404Page/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            <Route path="*" element={<Error404Page/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </BrowserRouter>
